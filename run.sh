13710a2ced0344f490a848aadc33e3a90e5a722d
Elliot Chance <elliotchance@gmail.com>
2017-03-12 15:59:17 +1100

    sqlite 3.17 now running through a container

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-DOCKER_TAG=sql:$1
+DOCKER_TAG=sqltest:$1
 
+# Rebuild the docker image. We need to copy the runner to the base folder so it
+# can be loaded into the image.
 docker build -t $DOCKER_TAG ${1//.//}
-docker run -d -p 5432:5432 --name $1 -v "`pwd`:/tmp/sql" $DOCKER_TAG -d pos || true
 
-python generate_tests.py $1
+# Run the container, we always run a fresh one.
+docker rm $(docker ps -aqf "name=$1") || true
+docker run -d --name $1 -v "`pwd`:/tmp/sql" $DOCKER_TAG \
+    /bin/sh -c "cd /tmp/sql; python generate_tests.py $1"
